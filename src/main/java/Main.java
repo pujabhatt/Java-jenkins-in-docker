f0544e2827c62ca1fbc7fa4cdfc2d4ef407e8fb8
pujabhatt1@gmail.com <pujabhatt1@gmail.com>
2022-11-21 00:40:03 -0800

    new line added to check jenkins trigger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing in the main method
+        System.out.println("Need line added to check trigger in jenkins");
     }
 
     public static boolean checkIfInputIsAnEvenNumber(int number){
